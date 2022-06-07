433039193ee6fd76864fd64aeb244cbc6384b5ac
DESKTOP-2C2FMSN\c0s1 <Carlos Montoya>
2022-06-07 16:47:43 -0500

    Advanced 16:47

--- backend/main.js
+++ backend/main.js
@@ -2,14 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         watch: 0,
-        user: [{
-            rol: "administrator", password: "1111",
-            rol: "secretary", password: "2222",
-            rol: "seller", password: "3333",
-            rol: "assembler", password: "4444",
+        users: [{
+            id: 1, rol: "administrator", password: "1111", salary: 5500000,
+            id: 2, rol: "secretary", password: "2222", salary: 2200000,
+            id: 3, rol: "seller", password: "3333", salary: 1800000,
+            id: 4, rol: "assembler", password: "4444", salary: 1500000,
         }],
+        user: 0,
+        baseSalary:0,
     },
     methods: {
+        modifySalary: function(){
+            this.users.array.forEach(element => {
+                if (this.user == 0) {
+                    alert("Seleccione un cargo") 
+                 } else if(this.user == element.id){
+                    element.salary = this.baseSalary;
+                 } 
+            });
+            
+        },
         showComponent: function () {
             
         }
