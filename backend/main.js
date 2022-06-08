fa90688712171ef49709231f017567d2e53a79c4
DESKTOP-2C2FMSN\c0s1 <Carlos Montoya>
2022-06-08 15:31:20 -0500

    Commit made to show progress 15:30 8-jun-22

--- backend/main.js
+++ backend/main.js
@@ -8,12 +8,43 @@ var app = new Vue({
             id: 3, rol: "seller", password: "3333", salary: 1800000,
             id: 4, rol: "assembler", password: "4444", salary: 1500000,
         }],
-        user: 0,
+        user: "",
         baseSalary:0,
+        maxShoes: "",
+        maxSlippers: "",
+        priceShoes: "",
+        priceSlippers: "",
+        commissionSeller: "",
+        totalSalarySecretary: "",
+        priceHourSecretary: "",
+        hoursExtraSecretary: "",
+        commissionSales: "",
+        salesMade: "",
+        subsidyTransport: 117.172
+
+
     },
     methods: {
+        showSalary: function() {
+            // if (this.user === "") {
+            //     alert("Seleccione cargo")
+            // } else if (this.user === "1") {
+            //     alert(this.baseSalary)
+            // } else {
+            //     alert("Hola")
+            // }
+            
+            this.users.forEach(element => {
+                if (this.user === "") {
+                    alert("Seleccione un cargo")
+                } else if(this.user === element.id){ 
+                    this.baseSalary = element.salary
+                    alert(element.salary)
+                }
+            });
+        },
         modifySalary: function(){
-            this.users.array.forEach(element => {
+            this.users.forEach(element => {
                 if (this.user == 0) {
                     alert("Seleccione un cargo") 
                  } else if(this.user == element.id){
@@ -22,8 +53,37 @@ var app = new Vue({
             });
             
         },
+        sendPriceAssembly: function () {
+            maxShoes = this.maxShoes
+            maxSlippers = this.maxSlippers
+            priceShoes = this.priceShoes
+            priceSlippers = this.priceSlippers            
+        },
+        calculateSecretary: function () {
+            let netSalarySecretary = 0
+           const search = users.find(element => element.id == 2)
+           this.priceHourSecretary = (search.salary / 30) / 8
+           netSalarySecretary = this.hoursExtraSecretary * 1.80
+           totalSalarySecretary = search.salary + netSalarySecretary           
+        },
+        calculateSeller: function () {
+            let bonus = 0
+            const search = users.find(element => element.id == 3)
+            if(this.salesMade >= 5000000){
+                bonus = search.salary * 0.1
+            } else if(this.salesMade >= 10000000 ){
+                bonus = search.salary * 0.2
+            }
+            totalSalarySeller = this.commissionSales + bonus + this.subsidyTransport
+        },
+        calculateAssembly: function () {
+            
+        },
         showComponent: function () {
             
         }
+    },
+    computed: {
+        
     }
   })
